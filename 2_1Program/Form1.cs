f40ec78d0ef17d521799e291851060084b4d418c
BorysFursov <37802805+BorysFursov@users.noreply.github.com>
2018-06-26 14:59:12 +0300

    Initial commit

---
+++ 2_1Program/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_1Program
+{
+    public partial class Form1 : Form
+    {
+        string Input;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Input = textBox1.Text;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Logics.GetResult(Input));
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 2_1Program/Logics.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_1Program
+{
+    class Logics
+    {   
+        private static string[] parseString(string str)
+        {
+            var separators = new string[] { ", " };
+            var res = str.Split(separators, StringSplitOptions.None);
+            return res;
+        }
+        public static string GetResult(string str)
+        {
+            var result = "";
+            string[] arr = parseString(str);
+            if(arr.Length == 1)
+            {
+                try
+                {
+                    result = "result of cubing is :"+ Math.Pow(Int32.Parse(arr[0]), 3).ToString();
+                }
+                catch (Exception ex)
+                {
+
+                    result = ex.Message;
+                }
+                return result;
+            }
+            else
+            {
+                var min = Int32.MaxValue;
+                var max = Int32.MinValue;
+                int[] intRes = new int[arr.Length];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    try
+                    {
+                        intRes[i] = Int32.Parse(arr[i]);
+                    }
+                    catch (Exception ex)
+                    {
+
+                        return ex.Message;
+                    }
+                }
+                foreach (var item in intRes)
+                {
+                    if (min > item)
+                    {
+                        min = item;
+                    }
+                    if( max < item)
+                    {
+                        max = item;
+                    }
+                }
+                return "minimum :" + min + "maximum:" + max; 
+            }
+        }
+    }
+}
